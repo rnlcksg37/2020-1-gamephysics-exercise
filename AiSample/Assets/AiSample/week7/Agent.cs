bf646af3ed1e65bed779c32a44f89b21cc5bfc90
Jae Beom Kim <jbkim1@vision.hoseo.edu>
2020-04-28 21:32:41 +0900

    코드 정리, 추격하기 환경 추가.
    주차별 코드 정리함.
    
    추격하기 환경 추가됨.
    
    - 추격하기 로직 미포함.

--- AiSample/Assets/AiSample/week7/Agent.cs
+++ AiSample/Assets/AiSample/week7/Agent.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputControlW6 : MonoBehaviour
+public class Agent : MonoBehaviour
 {
-    [SerializeField]
-    private Transform _agent = null;
-
     private Vector3 _pickPos = Vector3.zero;
     private Vector3 _fleePos = Vector3.zero;
 
@@ -16,12 +13,16 @@ public class InputControlW6 : MonoBehaviour
     [SerializeField]
     private float _deceleration = 1.0f;
 
+    private bool _isSeek = false;
+
     private Vector3 _velocity = Vector3.zero;
 
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
+            _isSeek = true;
+
             Vector3 mouse_pos = Input.mousePosition;
 
             RaycastHit hit;
@@ -44,19 +45,12 @@ public class InputControlW6 : MonoBehaviour
             }
         }
 
-        //if (Vector3.Distance(_agent.transform.position, _fleePos) < 10.0f)
-        //{
-        //    _velocity = _velocity + (flee(_fleePos) * Time.deltaTime);
-        //}
-
         // seek
-        //_velocity = _velocity + seek(_pickPos) * Time.deltaTime;
-
-        // arrive
-        _velocity = _velocity + arrive(_pickPos);
+        // 3: 적당히 빠르게 해주기 위함.
+        _velocity = _velocity + (seek(_pickPos) * Time.deltaTime * 3);
 
         // 속도를 기반으로 새로운 위치 계산.
-        _agent.transform.position = _agent.transform.position + (_velocity * Time.deltaTime);
+        transform.position = transform.position + _velocity;
     }
 
     private void OnDrawGizmos()
@@ -70,7 +64,14 @@ public class InputControlW6 : MonoBehaviour
 
     private Vector3 seek(Vector3 target_pos)
     {
-        Vector3 desired_velocity = ((target_pos - _agent.transform.position).normalized) * _maxSpeed;
+        // 방향 변경을 위함.
+        Vector3 distance = target_pos - transform.position;
+        if (distance.sqrMagnitude > 0.005f)
+        {
+            transform.forward = distance.normalized;
+        }
+
+        Vector3 desired_velocity = distance.normalized * _maxSpeed;
 
         return (desired_velocity - _velocity);
     }
@@ -78,18 +79,18 @@ public class InputControlW6 : MonoBehaviour
     private Vector3 flee(Vector3 target_pos)
     {
         // seek의 반대 방향 사용.
-        Vector3 desired_velocity = ((_agent.transform.position - target_pos).normalized) * _maxSpeed;
+        Vector3 desired_velocity = ((transform.position - target_pos).normalized) * _maxSpeed;
 
         return (desired_velocity - _velocity);
     }
 
     private Vector3 arrive(Vector3 target_pos)
     {
-        float distance = Vector3.Distance(target_pos, _agent.transform.position);
+        float distance = Vector3.Distance(target_pos, transform.position);
 
         if (distance > 0.0f)
         {
-            Vector3 to_target = target_pos - _agent.transform.position;
+            Vector3 to_target = target_pos - transform.position;
 
             float _speed = distance / _deceleration;
 
---
+++ AiSample/Assets/AiSample/week7/AgentPursuit.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentPursuit : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target = null;
+
+    [SerializeField]
+    private float _maxSpeed = 0.2f;
+
+    private bool _isPursuit = false;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _isPursuit = true;
+        }
+
+        if (_isPursuit)
+        {
+            // pursuit
+            // 3: 적당히 빠르게 해주기 위함.
+            _velocity = _velocity + pursuit(_target);
+
+            // 속도를 기반으로 새로운 위치 계산.
+            transform.position = transform.position + _velocity;
+        }
+    }
+
+    private Vector3 pursuit(Transform target_agent)
+    {
+        return seek(target_agent.position);
+    }
+
+    private Vector3 seek(Vector3 target_pos)
+    {
+        // 방향 변경을 위함.
+        Vector3 dir = (target_pos - transform.position).normalized;
+        if (dir.sqrMagnitude > 0.0f)
+        {
+            transform.forward = dir;
+        }
+
+        Vector3 desired_velocity = dir * _maxSpeed;
+
+        return (desired_velocity - _velocity);
+    }
+}
